7602bc700bc796eba9be4fa4fe1a8697745ddc47
Jeromqw1 <afayizu@gmail.com>
2023-07-13 20:55:59 +0000

    Update

---
+++ push_to_github.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Change directory to my local Git repository
+cd /home/phaceless/alx-pre_course/simple_shell
+
+# Add all changes to the staging area
+git add .
+
+# Add a commit message
+git commit -m "Update"
+
+# Push the changes to my repository
+git push origin master

f4f23756990c5829408d2a41778f172a1ad4da17
Jeromqw1 <afayizu@gmail.com>
2023-07-13 21:03:54 +0000

    class

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Change directory to my local Git repository
-cd https://github.com/Jeromqw1/simple_shell
+cd /home/phaceless/alx-pre_course/simple_shell
+
+# Prompt the user for a commit message
+read -p "Enter your commit message: " commit_message
 
 # Add all changes to the staging area
 git add .
 
 # Add a commit message
-git commit -m "Update"
+git commit -m "$commit_message"
 
 # Push the changes to my repository
 git push origin master
